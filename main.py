0148b9de3ec0f335d280da8999988432d84908bb
zzanzu <lees3301@gmail.com>
2018-05-11 15:39:25 +0900

    read words from text file and save to list : 텍스트 파일의 단어들을 리스트에 저장 : TODO - 중복 단어 제거

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-def convert_text8_to_vector() :
-    data = open('./text8.txt')
-    
+import numpy as np
+
+class Word2Vec() :
+
+    """ 텍스트 파일의 단어들을 리스트로 바꿔줌 """
+    # TODO 2018-05-11 15:37 중복 단어 제거
+    def read_words(self, words_file):
+        with open(words_file, 'r') as f:
+            ret = []
+            for line in f:
+                ret += line.split()
+            return ret
+
+
+if __name__ == "__main__":
+    word2vec = Word2Vec()
+
+    print(word2vec.read_words('text8.txt'))
