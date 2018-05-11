f244133507ffc0c89df20ba69f76cacb60d3f22d
zzanzu <lees3301@gmail.com>
2018-05-11 15:50:30 +0900

    add function for deleting duplicating element in word list : 단어 리스트에서 중복된 단어 삭제 기능 추가(list의 set 사용) : TODO - 이렇게 생성한 단어 리스트의 원소 하나씩 one-hot 벡터로 바꿔주기

--- main.py
+++ main.py
@@ -3,13 +3,20 @@ import numpy as np
 class Word2Vec() :
 
     """ 텍스트 파일의 단어들을 리스트로 바꿔줌 """
-    # TODO 2018-05-11 15:37 중복 단어 제거
     def read_words(self, words_file):
         with open(words_file, 'r') as f:
             ret = []
             for line in f:
                 ret += line.split()
+
+            ret = list(set(ret)) # DONE 2018-05-11 15:37 중복 단어 제거
+
             return ret
+
+    """ 단어를 가진 리스트의 원소 하나씩 꺼내서 one-hot vector로 변환 """
+    # TODO 2018-05-11 15:47 각 단어들을 one-hot encoding 하기
+    def convert_to_vector(self):
+
 
 
 if __name__ == "__main__":
