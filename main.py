84959551c028d9dd4c92034be54480c2353e7689
zzanzu <lees3301@gmail.com>
2018-05-17 22:43:30 +0900

    add getting frequency of words and save as dictionary : text8에서 단어들을 읽어올때 중복을 제거하지 않고, 읽어온 단어들을 가공(frequency 구하기, indexing하기 등)할 때 함수 내부에서 자체적으로 중복을 제거하도록 변경

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class Word2Vec() :
             for line in f:
                 ret += line.split()
 
-            ret = list(set(ret)) # DONE 2018-05-11 15:37 중복단어제거
+            # ret = list(set(ret)) # DONE 2018-05-11 15:37 중복단어제거
 
             return ret
 
@@ -19,12 +19,39 @@ class Word2Vec() :
         for word in word_list:
              print(word)
 
+    ''' 단어를 indexing '''
+    def indexing_words(self, word_list):
+        word_list = list(set(word_list)) # 중복제거
 
+        indexed_word_dict = {} # 단어별로 indexing된 dictionary 선언
 
+        i = 0
+        for word in word_list:
+            indexed_word_dict[word] = i
+            i += 1
+
+        print(indexed_word_dict)
+
+        return indexed_word_dict
+
+    ''' 단어별로 빈도 수를 세어서 dictionary 생성 '''
+    def get_frequency_words(self, word_list):
+
+        word_frequency_dict = {}
+
+        # 중복제거된 단어 리스트에서 빈도 수를 세어서 dictionary에 저장
+        for word in list(set(word_list)):
+            word_frequency_dict[word] = word_list.count(word)
+
+        print(word_frequency_dict)
+
+        return word_frequency_dict
 
 if __name__ == "__main__":
     word2vec = Word2Vec()
     word_list = word2vec.read_words('text8.txt')
+    # word2vec.indexing_words(word_list)
+    word2vec.get_frequency_words(word_list)[0]
 
     # print(len(word_list)) # 17005207개
-    word2vec.convert_to_vector(word_list)
+    # word2vec.convert_to_vector(word_list)
